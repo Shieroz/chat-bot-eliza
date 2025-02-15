f7ec6f7fb3ca521ddd3b61dbfc4c7bff51736f65
Phong Nguyen <shieroz@mac.lan>
2025-02-14 22:23:45 -0500

    Refactor and add some keyword handling ability to eliza

--- eliza.py
+++ eliza.py
@@ -1,33 +1,39 @@
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import json
+import random
+import sys
 
 tokenizer = RegexpTokenizer(r'\w+')
 lemmatizer = WordNetLemmatizer()
-first = True
+
+# Remember important details about the user
+memory = dict()
+
+# Load the responses of eliza
+with open('./keywords.json', 'r') as file:
+    data = json.load(file)
+
+# Reverse dictionary to map from keywords to theme
+keyword_map = dict()
+for theme in data:
+    if "keywords" in data[theme]:
+        for word in data[theme]["keywords"]:
+            keyword_map[word] = theme
 
 def eliza():
-    """ Main function for the Eliza chatbot """
-    global first
+    """ Main program loop for the Eliza chatbot """
     while True:
-        user_input = input()
-        if user_input.lower() == "goodbye":
-            print("Goodbye! Have a nice day!")
-            break
-        clean_text = data_cleaning(user_input)
-
-        if first:
-            name = clean_text[-1]
-            print(f"Hi {name}. How can I help you today?")
-            first = False
-
+        # Get user input and clean it
+        user_input = input("> ")
+        if user_input == "":
+            print(data["EMPTY"][random.randrange(0, len(data["EMPTY"]))])
         else:
-            clean_text = data_cleaning(user_input)
-            print(clean_text)
-            
-            print("I'm sorry to hear that. Can you tell me more about why you're feeling that way?")
+            clean_text = _data_cleaning(user_input)
+            _respond(clean_text)
 
-def data_cleaning(text: str) -> list:
+def _data_cleaning(text: str) -> list:
     """ Clean up the text data by removing punctuations stopwords and lemmatize nouns and verbs"""
     # Clean up punctuations and convert to lowercase
     tokens = tokenizer.tokenize(text.lower())
@@ -41,6 +47,23 @@ def data_cleaning(text: str) -> list:
 
     return tokens
 
+def _respond(input: list):
+    # Find keywords in input
+    keywords = dict()
+    for word in input:
+        if word in keyword_map:
+            keywords[word] = keyword_map[word]
+
+    # If no keywords are detected defaults to NULL responses
+    if not keywords:
+        print(data["NULL"][random.randrange(0, len(data["NULL"]))])
+    # Handle program exit
+    elif "quit" in keywords.values():
+        sys.exit(data["quit"]["keywords"][random.randrange(0, len(data["quit"]["keywords"]))])
+    else:
+        print("")
+
 if __name__ == "__main__":
+    print("Type \"goodbye\" to quit.")
     print("Hi, I'm a psychotherapist. What is your name?")
     eliza()
